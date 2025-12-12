b92e932a5fad090a5e1583cf02b4cc1ba982d52e
Bpost129 <Bpost129@yahoo.com>
2025-12-11 23:53:05 -0500

    add ejs view for home page and render, import ejs and set view engine

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express()
 
 // configure the app (app.set)
 
-
+app.set('view engine', 'ejs')
 
 // mount Middleware (app.use)
 
@@ -21,7 +21,7 @@ app.get('/', function(req, res) {
 })
 
 app.get('/home', function(req, res) {
-  res.send('<h1>Home Page</h1>')
+  res.render('home')
 })
 
 
